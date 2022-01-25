869153403af3a606ab12eb463662c573c174d11b
Kin Ian Lo <keonlo123@gmail.com>
2022-01-25 11:58:43 +0000

    Implement incidence matrix method

--- scenario.py
+++ scenario.py
@@ -1,10 +1,19 @@
+import numpy
+import itertools
+
 class Scenario:
     def __init__(self, contexts: list[tuple[str, ...]], num_outcome: int):
         self.contexts = contexts
         self.num_outcome = num_outcome
 
+        self._observables = None
+        self._incidence_matrix = None
+
     @property
     def observables(self) -> list[str]:
+        if self._observables:
+            return self._observables
+
         out = []
         for context in self.contexts:
             for observable in context:
@@ -12,8 +21,33 @@ class Scenario:
                     out.append(observable)
         return out
 
+    @property
+    def incidence_matrix(self) -> numpy.ndarray:
+        if self._incidence_matrix:
+            return self._incidence_matrix
+        global_assigns = list(itertools.product(range(self.num_outcome), repeat=len(self.observables)))
+
+        M_context = []
+        for context in self.contexts:
+            local_assigns = list(itertools.product(range(self.num_outcome), repeat=len(context)))
+
+            ob_idx = [self.observables.index(ob) for ob in context]
+            g_context = [tuple([g[idx] for idx in ob_idx]) for g in global_assigns]
+
+            M_context_this = numpy.zeros((len(local_assigns), len(global_assigns)), dtype=int)
+            for i in range(len(local_assigns)):
+                for j in range(len(g_context)):
+                    M_context_this[i, j] = local_assigns[i] == g_context[j]
+
+            M_context.append(M_context_this)
+
+        M = numpy.vstack(M_context)
+        return M
+
+
 class CyclicScenario(Scenario):
     def __init__(self, observables: list[str], num_outcome: int):
+        self._observables = observables
         contexts = []
         for i in range(len(observables)):
             i_next = (i + 1) % len(observables)
@@ -21,4 +55,6 @@ class CyclicScenario(Scenario):
 
         super().__init__(contexts, num_outcome)
 
-
+if __name__ == "__main__":
+    s = CyclicScenario(['x1', 'x2', 'x3'], 2)
+    print(s.incidence_matrix)
