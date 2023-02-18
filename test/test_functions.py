0e7e45cd298551bf6f66c86d0ea8da0b7f4bfbae
Kin Ian Lo <keonlo123@gmail.com>
2023-02-18 16:05:08 +0000

    update tests

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,5 +1,4 @@
-from random import random
-
+import pytest
 
 def test_print_model_cbd():
     from contextuality import chsh_scenario, random_model, print_model_cbd
@@ -10,9 +9,23 @@ def test_print_model_cbd():
 
 def test_symmetrized_model():
     from contextuality import symmetrized_model
-    from contextuality import pr_model, random_pr_like_model
+    from contextuality import pr_model, random_pr_like_model, random_model, chsh_scenario
+    from itertools import product
+
     for n in range(3, 10):
         assert symmetrized_model(pr_model(n)) == pr_model(n)
         assert symmetrized_model(random_pr_like_model(n)) == pr_model(n)
 
+    for _ in range(10):
+        scenario = chsh_scenario()
+        model = symmetrized_model(random_model(scenario))
+        for context in scenario.contexts:
+            for outcome in product((0, 1), repeat=2):
+                sym_outcome = tuple(1-out for out in outcome)
+                prob = model.probability(context, outcome)
+                sym_prob = model.probability(context, sym_outcome)
+                assert prob == pytest.approx(sym_prob)
+                
+
+        
     
